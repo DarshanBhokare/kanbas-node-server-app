31b3bb0adacdb1167f77c67ef147a71be8d30432
DarshanBhokare <bhokare.d@northeastern.edu>
2023-12-04 21:53:59 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './lab5.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import AssignmentRoutes from './assignments/routes.js';
+import UserRoutes from './users/routes.js';
+import session from 'express-session';
 import 'dotenv/config';
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
@@ -13,10 +21,28 @@ app.use(
     origin: process.env.FRONTEND_URL,
   })
 );
+
+const sessionOptions = {
+  secret: 'any string',
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: 'none',
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000);
